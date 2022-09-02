887f6b8da00326328e091163df2d71061ed75c24
juanpablovj <juanpablovj2030@gmail.com>
2022-09-01 22:07:57 -0500

    Demo deployment

---
+++ components/layouts/DarkLayout.tsx
@@ -0,0 +1,16 @@
+import React, { FC, PropsWithChildren } from "react";
+
+export const DarkLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "rgba(255,0,0,0.3)",
+        borderRadius: "5px",
+        padding: "10px",
+      }}
+    >
+      <h3>Dark Layout</h3>
+      {children}
+    </div>
+  );
+};
---
+++ components/layouts/MainLayout.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import { FC, PropsWithChildren } from "react";
+
+import { Navbar } from "../Navbar";
+import styles from "./MainLayout.module.css";
+
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="description" content="Home page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
